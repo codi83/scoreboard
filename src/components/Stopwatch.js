4c5d56e09c26231e79653a035b79aa53763c17e6
hercules <codi83@naver.com>
2019-03-10 10:30:37 +0900

    module breabing4: submit

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+
+export class Stopwatch extends React.Component{
+  tickRef;
+
+  state = {
+    isRunning: false,
+    timer : 0
+  }
+
+  tick = () => {
+    if(this.state.isRunning){
+      this.setState(prevState => ({timer: prevState.timer + 1}));
+    }
+  }
+
+  handleStopwatch = () =>{
+    this.setState(prevState => ({isRunning: !prevState.isRunning}));
+  }
+
+  handleReset =()=>{
+    this.setState({timer:0});
+  }
+
+  render()
+  {
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <sapn className="stopwatch-time">{this.state.timer}</sapn>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
+        <button>Reset</button>
+      </div>
+    );
+  }
+
+  //DOM이 렌더링된 직수
+  componentDidMount() {
+    this.tickRef = setInterval(this.tick, 1000);
+  }
+
+  //파괴되지 직전 호출
+  componentWillUnmount() {
+    clearInterval(this.tickRef);
+  }
+}
